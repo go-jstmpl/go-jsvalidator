89f6e76b2f166c1c10f2a81719058af1570c249b
fujiwara402 <fuj.t601216@gmail.com>
2016-08-22 20:04:09 +0900

    Refactoring validation helper

--- templates/validation_helper.go
+++ templates/validation_helper.go
@@ -5,6 +5,10 @@ import (
 	"regexp"
 )
 
+/*
+if "exclusiveMaximum" is not present, or has boolean value false, then the instance is valid if it is lower than, or equal to, the value of "maximum";
+if "exclusiveMaximum" has boolean value true, the instance is valid if it is strictly lower than the value of "maximum".
+*/
 type MaximumError struct {
 	Expected  int  `json:"expected"`
 	Actual    int  `json:"actual"`
@@ -15,6 +19,11 @@ func (m MaximumError) Error() string {
 	return fmt.Sprintf("expected less than %d but actual %d with option exlusive %b", m.Expected, m.Actual, m.Exclusive)
 }
 
+/*
+if "exclusiveMinimum" is not present, or has boolean value false, then the instance is valid if it is greater than, or equal to, the value of "minimum";
+
+if "exclusiveMinimum" is present and has boolean value true, the instance is valid if it is strictly greater than the value of "minimum".
+*/
 type MinimumError struct {
 	Expected  int  `json:"expected"`
 	Actual    int  `json:"actual"`
@@ -61,37 +70,39 @@ func (m EnumError) Error() string {
 	return fmt.Sprintf("", m.Expected, m.Actual)
 }
 
-func Maximum(val int, max int, exc bool) error {
+func Maximum(val, max int, exc bool) error {
 	if exc {
 		if val >= max {
 			return MaximumError{max, val, exc}
 		}
-	} else {
-		if val > max {
-			return MaximumError{max, val, exc}
-		}
+		return nil
+	}
+
+	if val > max {
+		return MaximumError{max, val, exc}
 	}
 
 	return nil
 }
 
-func Minimum(val int, min int, exc bool) error {
+func Minimum(val, min int, exc bool) error {
 	if exc {
 		if val <= min {
 			return MinimumError{min, val, exc}
 		}
-	} else {
-		if val < min {
-			return MinimumError{min, val, exc}
-		}
+		return nil
+	}
+
+	if val < min {
+		return MinimumError{min, val, exc}
 	}
 
 	return nil
 }
 
 func MaxLength(val string, max int) error {
-	if max == 0 {
-		return nil
+	if max < 0 {
+		return fmt.Errorf("maxLength must be greater than, or equal to, 0. maxLength: %d", max)
 	}
 
 	c := len(val)
@@ -103,9 +114,10 @@ func MaxLength(val string, max int) error {
 }
 
 func MinLength(val string, min int) error {
-	if min == 0 {
-		return nil
+	if min < 0 {
+		return fmt.Errorf("minLength must be greater than, or equal to, 0. minLenght: %d", min)
 	}
+
 	c := len(val)
 	if c < min {
 		return MinLengthError{min, c}
@@ -114,20 +126,36 @@ func MinLength(val string, min int) error {
 	return nil
 }
 
-func Pattern(val string, regx string) error {
-	if regx == "" {
-		return nil
+func Pattern(val, regx string) error {
+	ok, err := regexp.MatchString(regx, val)
+	if err != nil {
+		return err
 	}
 
-	ok, err := regexp.MatchString(regx, val)
-	if err != nil || ok != true {
+	if ok {
 		return PatternError{regx, val}
 	}
 
 	return nil
 }
 
-func Enum(str string, accepts ...string) error {
+func Enum(str string, accepts []string) error {
+	if len(accepts) < 1 {
+		return fmt.Errorf("enum must have at least one element. enum element count:%d", len(accepts))
+	}
+
+	for i, e := range accepts {
+		if i == len(accepts)-1 {
+			break
+		}
+
+		for j := i; j < len(accepts); i++ {
+			if e == accepts[j+1] {
+				return fmt.Errorf("elements in the enum must be unique. `%s`", accepts)
+			}
+		}
+	}
+
 	for _, v := range accepts {
 		if str == v {
 			return nil
