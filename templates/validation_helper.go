ede5623f65bde7095491900e7be31336f79f133b
fujiwara402 <fuj.t601216@gmail.com>
2016-08-23 16:34:10 +0900

    Refactoring vaildation helper

--- templates/validation_helper.go
+++ templates/validation_helper.go
@@ -5,10 +5,6 @@ import (
 	"regexp"
 )
 
-/*
-if "exclusiveMaximum" is not present, or has boolean value false, then the instance is valid if it is lower than, or equal to, the value of "maximum";
-if "exclusiveMaximum" has boolean value true, the instance is valid if it is strictly lower than the value of "maximum".
-*/
 type MaximumError struct {
 	Expected  int  `json:"expected"`
 	Actual    int  `json:"actual"`
@@ -19,11 +15,6 @@ func (m MaximumError) Error() string {
 	return fmt.Sprintf("expected less than %d but actual %d with option exlusive %b", m.Expected, m.Actual, m.Exclusive)
 }
 
-/*
-if "exclusiveMinimum" is not present, or has boolean value false, then the instance is valid if it is greater than, or equal to, the value of "minimum";
-
-if "exclusiveMinimum" is present and has boolean value true, the instance is valid if it is strictly greater than the value of "minimum".
-*/
 type MinimumError struct {
 	Expected  int  `json:"expected"`
 	Actual    int  `json:"actual"`
@@ -70,6 +61,8 @@ func (m EnumError) Error() string {
 	return fmt.Sprintf("", m.Expected, m.Actual)
 }
 
+//if "exclusiveMaximum" is not present, or has boolean value false, then the instance is valid if it is lower than, or equal to, the value of "maximum";
+//if "exclusiveMaximum" has boolean value true, the instance is valid if it is strictly lower than the value of "maximum".
 func Maximum(val, max int, exc bool) error {
 	if exc {
 		if val >= max {
@@ -85,6 +78,8 @@ func Maximum(val, max int, exc bool) error {
 	return nil
 }
 
+//if "exclusiveMinimum" is not present, or has boolean value false, then the instance is valid if it is greater than, or equal to, the value of "minimum";
+//if "exclusiveMinimum" is present and has boolean value true, the instance is valid if it is strictly greater than the value of "minimum".
 func Minimum(val, min int, exc bool) error {
 	if exc {
 		if val <= min {
@@ -126,41 +121,38 @@ func MinLength(val string, min int) error {
 	return nil
 }
 
-func Pattern(val, regx string) error {
-	ok, err := regexp.MatchString(regx, val)
+func Pattern(val, pattern string) error {
+	ok, err := regexp.MatchString(pattern, val)
 	if err != nil {
 		return err
 	}
 
-	if ok {
-		return PatternError{regx, val}
+	if ok == false {
+		return PatternError{pattern, val}
 	}
 
 	return nil
 }
 
-func Enum(str string, accepts []string) error {
-	if len(accepts) < 1 {
-		return fmt.Errorf("enum must have at least one element. enum element count:%d", len(accepts))
+func Enum(val string, enum []string) error {
+	if len(enum) < 1 {
+		return fmt.Errorf("enum must have at least one element. enum element count:%d", len(enum))
 	}
 
-	for i, e := range accepts {
-		if i == len(accepts)-1 {
-			break
-		}
-
-		for j := i; j < len(accepts); i++ {
-			if e == accepts[j+1] {
-				return fmt.Errorf("elements in the enum must be unique. `%s`", accepts)
+	// element in the enum must be unique.
+	for i, e := range enum {
+		for j := i + 1; j < len(enum); j++ {
+			if e == enum[j] {
+				return fmt.Errorf("elements in the enum must be unique. `%s`", enum)
 			}
 		}
 	}
 
-	for _, v := range accepts {
-		if str == v {
+	for _, e := range enum {
+		if val == e {
 			return nil
 		}
 	}
 
-	return EnumError{accepts, str}
+	return EnumError{enum, val}
 }
--- templates/validation_helper_test.go
+++ templates/validation_helper_test.go
@@ -7,139 +7,102 @@ import (
 func TestMaximum(t *testing.T) {
 	err := Maximum(100, 200, true)
 	if err != nil {
-		t.Log(err, "100, 200, true")
-		t.Fail()
-	}
-	err = Maximum(200, 200, true)
-	if err == nil {
-		t.Log(err, "200, 200, true")
-		t.Fail()
-	}
-	err = Maximum(100, 200, false)
-	if err != nil {
-		t.Log(err, "100, 200, false")
-		t.Fail()
-	}
-	err = Maximum(200, 200, false)
-	if err != nil {
-		t.Log(err, "200, 200, false")
-		t.Fail()
-	}
-	err = Maximum(0, 0, false)
-	if err != nil {
-		t.Log(err, "0, 0, false")
-		t.Fail()
-	}
-	err = Maximum(0, 0, true)
-	if err == nil {
-		t.Log(err, "0, 0, true")
-		t.Fail()
+		t.Error(err.Error())
 	}
 }
 
 func TestMinimum(t *testing.T) {
 	err := Minimum(100, 10, true)
 	if err != nil {
-		t.Log(err, "100, 10, true")
-		t.Fail()
+		t.Error(err)
 	}
+
 	err = Minimum(200, 200, true)
 	if err == nil {
-		t.Log(err, "200, 200, true")
-		t.Fail()
+		t.Error(err)
 	}
+
 	err = Minimum(100, 10, false)
 	if err != nil {
-		t.Log(err, "100, 10, false")
-		t.Fail()
+		t.Error(err)
 	}
+
 	err = Minimum(200, 200, false)
 	if err != nil {
-		t.Log(err, "200, 200, false")
-		t.Fail()
+		t.Error(err)
 	}
+
 	err = Minimum(0, 0, false)
 	if err != nil {
-		t.Log(err, "0, 0, false")
-		t.Fail()
+		t.Error(err)
 	}
+
 	err = Minimum(0, 0, true)
 	if err == nil {
-		t.Log(err, "0, 0, true")
-		t.Fail()
+		t.Error(err)
 	}
 }
 
 func TestMaxLength(t *testing.T) {
-	err := MaxLength("hogehoge", 10)
-	if err != nil {
-		t.Fail()
+	err := MaxLength("hogehoge", -1)
+	if err == nil {
+		t.Error(err)
 	}
 
 	err = MaxLength("hogehoge", 8)
 	if err != nil {
-		t.Fail()
+		t.Error(err)
 	}
 
 	err = MaxLength("hogehoge", 6)
 	if err == nil {
-		t.Fail()
+		t.Error(err)
 	}
 
-	err = MaxLength("hogehoge", 0)
+	err = MaxLength("hogehoge", 1111)
 	if err != nil {
-		t.Fail()
+		t.Error(err)
 	}
 }
 
 func TestMinLength(t *testing.T) {
 	err := MinLength("hogehoge", 6)
 	if err != nil {
-		t.Fail()
+		t.Error(err)
 	}
 
 	err = MinLength("hogehoge", 8)
 	if err != nil {
-		t.Fail()
+		t.Error(err)
 	}
 
 	err = MinLength("hogehoge", 10)
 	if err == nil {
-		t.Fail()
+		t.Error(err)
 	}
 
 	err = MinLength("hogehoge", 0)
 	if err != nil {
-		t.Fail()
+		t.Error(err)
 	}
 }
 
 func TestPattern(t *testing.T) {
-	err := Pattern("seafood", `foo.*`)
+	err := Pattern("2016-05-09T19:45:32Z", "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z$")
 	if err != nil {
-		t.Fail()
+		t.Error(err)
 	}
 
 	err = Pattern("", "")
 	if err != nil {
-		t.Fail()
+		t.Error(err)
 	}
 }
 
 func TestEnum(t *testing.T) {
-	en := []string{"red", "amber", "green"}
-	err := Enum("red", en...)
+	en := []string{"amber", "red", "blue"}
+	err := Enum("red", en)
 	if err != nil {
-		t.Fail()
-	}
-
-	err = Enum("blue", en...)
-	if err == nil {
-		t.Fail()
-	}
-
-	err = Enum("", en...)
-	if err == nil {
-		t.Fail()
+		t.Error(err)
 	}
 }
