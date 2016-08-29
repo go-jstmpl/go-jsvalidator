722074d5a313b7cd2d0d8e71f4a2903787761c15
fujiwara402 <fuj.t601216@gmail.com>
2016-08-29 15:40:43 +0900

    Refactoring validation helper

---
+++ validation_helper.go
@@ -0,0 +1,247 @@
+package validator
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"unicode/utf8"
+)
+
+type MaximumValidator struct {
+	Maximum   int  `json:"maximum"`
+	Exclusive bool `json:"exclusive"`
+}
+
+func (m MaximumValidator) Error(val int) string {
+	return fmt.Sprintf("expected less than %d but actual %d with option exlusive %b", m.Maximum, val, m.Exclusive)
+
+}
+
+func NewMaximumValidator(maximum int, exclusive bool) (*MaximumValidator, error) {
+	v := &MaximumValidator{
+		Maximum:   maximum,
+		Exclusive: exclusive,
+	}
+
+	return v, nil
+}
+
+//if "exclusiveMinimum" is not present, or has boolean value false, then the instance is valid if it is greater than, or equal to, the value of "minimum";
+//if "exlusiveMinimum" is present and has boolean value true, the instance is valid if it is strictly greater than the value of "minimum".
+func (m MaximumValidator) Validate(val int) bool {
+	if !m.Exclusive {
+		if val <= m.Maximum {
+			return true
+		}
+		return false
+	}
+
+	if val < m.Maximum {
+		return true
+	}
+	return false
+}
+
+type MinimumValidator struct {
+	Minimum   int  `json:"minimum"`
+	Exclusive bool `json:"exclusive"`
+}
+
+func (m MinimumValidator) Error(val int) string {
+	return fmt.Sprintf("expected greater than %d but actual %d with option exlusive %b", m.Minimum, val, m.Exclusive)
+}
+
+func NewMinimumValidator(minimum int, exclusive bool) (*MinimumValidator, error) {
+	v := &MinimumValidator{
+		Minimum:   minimum,
+		Exclusive: exclusive,
+	}
+
+	return v, nil
+}
+
+//if "exclusiveMinimum" is not present, or has boolean value false, then the instance is valid if it is greater than, or equal to, the value of "minimum";
+//if "exclusiveMinimum" is present and has boolean value true, the instance is valid if it is strictly greater than the value of "minimum".
+func (m MinimumValidator) Validate(val int) bool {
+	if !m.Exclusive {
+		if val >= m.Minimum {
+			return true
+		}
+		return false
+	}
+
+	if val > m.Minimum {
+		return true
+	}
+	return false
+}
+
+type MaxLengthValidator struct {
+	MaxLength int `json:"maxlength"`
+}
+
+func (m MaxLengthValidator) Error(val string) string {
+	return fmt.Sprintf("expected less than, or equal to, %d charactors but actual %d", m.MaxLength, utf8.RuneCountInString(val))
+}
+
+func NewMaxLengthValidator(maxLength int) (*MaxLengthValidator, error) {
+	if maxLength <= 0 {
+		return nil, fmt.Errorf("maxLength must be greater than 0. maxLength: %d", maxLength)
+	}
+	v := &MaxLengthValidator{
+		MaxLength: maxLength,
+	}
+
+	return v, nil
+}
+
+func (m MaxLengthValidator) Validate(val string) bool {
+	if utf8.RuneCountInString(val) <= m.MaxLength {
+		return true
+	}
+
+	return false
+}
+
+type MinLengthValidator struct {
+	MinLength int `json:"minLength"`
+}
+
+func (m MinLengthValidator) Error(val string) string {
+	return fmt.Sprintf("expected greater than, or equal to, %d charactors but actual %d charactors", m.MinLength, utf8.RuneCountInString(val))
+}
+
+func NewMinLengthValidator(minLength int) (*MinLengthValidator, error) {
+	if minLength <= 0 {
+		return nil, fmt.Errorf("minLength must be less than 0. minLength: %d", minLength)
+	}
+	v := &MinLengthValidator{
+		MinLength: minLength,
+	}
+
+	return v, nil
+}
+
+func (m MinLengthValidator) Validate(val string) bool {
+	if utf8.RuneCountInString(val) >= m.MinLength {
+		return true
+	}
+
+	return false
+}
+
+type PatternValidator struct {
+	Pattern string
+}
+
+func (m PatternValidator) Error(val string) string {
+	return fmt.Sprintf("%s expected matching pattern: %s", val, m.Pattern)
+}
+
+func NewPatternValidator(pattern string) (*PatternValidator, error) {
+	_, err := regexp.Compile(pattern)
+	if err != nil {
+		return nil, err
+	}
+	v := &PatternValidator{
+		Pattern: pattern,
+	}
+
+	return v, nil
+}
+
+func (m PatternValidator) Validate(val string) bool {
+	// need fix return value type is wrong
+	ok, err := regexp.MatchString(m.Pattern, val)
+	if err != nil {
+		return false
+	}
+
+	if ok == false {
+		return false
+	}
+
+	return true
+}
+
+type IntEnumValidator struct {
+	Enumerate []int
+}
+
+func (m IntEnumValidator) Error(val string) string {
+	return fmt.Sprintf("%s expected include emunerate list: %v", val, m.Enumerate)
+}
+
+func NewIntEnumValidator(enumerate []int) (*IntEnumValidator, error) {
+	l := enumerate
+
+	if len(l) == 0 {
+		return nil, errors.New("enumerate element should not be empty")
+	}
+
+	// unique test
+	for idx, e := range l {
+		for _, es := range l[idx+1:] {
+			if e == es {
+				return nil, errors.New("enumerate element should be unique")
+			}
+		}
+	}
+
+	v := &IntEnumValidator{
+		Enumerate: enumerate,
+	}
+
+	return v, nil
+}
+
+func (m IntEnumValidator) Validate(val int) bool {
+	for _, e := range m.Enumerate {
+		if val == e {
+			return true
+		}
+	}
+
+	return false
+}
+
+type StringEnumValidator struct {
+	Enumerate []string
+}
+
+func (m StringEnumValidator) Error(val string) string {
+	return fmt.Sprintf("%s expected include enumerate list: %v", val, m.Enumerate)
+}
+
+func NewStringEnumValidator(enumerate []string) (*StringEnumValidator, error) {
+	l := enumerate
+
+	if len(l) == 0 {
+		return nil, errors.New("enumerate element should not be empty")
+	}
+
+	// unique test
+	for idx, e := range l {
+		for _, es := range l[idx+1:] {
+			if e == es {
+				return nil, errors.New("enumerate element should be unique")
+			}
+		}
+	}
+
+	v := &StringEnumValidator{
+		Enumerate: enumerate,
+	}
+
+	return v, nil
+}
+
+func (m StringEnumValidator) Validate(val string) bool {
+	for _, e := range m.Enumerate {
+		if val == e {
+			return true
+		}
+	}
+
+	return false
+}
---
+++ validation_helper_test.go
@@ -0,0 +1,560 @@
+package validator
+
+import (
+	"testing"
+)
+
+//Structs for make constructer test case
+type MaximumTestCase struct {
+	Case    MaximumValidator
+	Message string
+	Pass    bool
+}
+
+type MinimumTestCase struct {
+	Case    MinimumValidator
+	Message string
+	Pass    bool
+}
+
+type MaxLengthTestCase struct {
+	Case    MaxLengthValidator
+	Message string
+	Pass    bool
+}
+
+type MinLengthTestCase struct {
+	Case    MinLengthValidator
+	Message string
+	Pass    bool
+}
+
+type PatternTestCase struct {
+	Case    PatternValidator
+	Message string
+	Pass    bool
+}
+
+type IntEnumTestCase struct {
+	Case    IntEnumValidator
+	Message string
+	Pass    bool
+}
+
+type StringEnumTestCase struct {
+	Case    StringEnumValidator
+	Message string
+	Pass    bool
+}
+
+//Structs for make validator test case
+type MaximumValidatorTestCase struct {
+	Case    int
+	Message string
+	Pass    bool
+}
+
+type MinimumValidatorTestCase struct {
+	Case    int
+	Message string
+	Pass    bool
+}
+
+type MaxLengthValidatorTestCase struct {
+	Case    string
+	Message string
+	Pass    bool
+}
+
+type MinLengthValidatorTestCase struct {
+	Case    string
+	Message string
+	Pass    bool
+}
+
+type PatternValidatorTestCase struct {
+	Case    string
+	Message string
+	Pass    bool
+}
+
+type IntEnumValidatorTestCase struct {
+	Case    int
+	Message string
+	Pass    bool
+}
+
+type StringEnumValidatorTestCase struct {
+	Case    string
+	Message string
+	Pass    bool
+}
+
+// The purpose of method with a name like 'Test"Validation Keyword"' is testing constructor
+// The purpose of method with a name like 'Test"Validation Keyword"Validator' is testing validator
+
+func TestMaximumValidator(t *testing.T) {
+	//Case exclusive is false
+	validator, err := NewMaximumValidator(100, false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	tests := []MaximumValidatorTestCase{{
+		Case:    99,
+		Message: "'99' is less than Maximum therefore should be pass",
+		Pass:    true,
+	}, {
+		Case:    100,
+		Message: "'100' equal to Maximum and exclusive is false in this case should be pass",
+		Pass:    true,
+	}, {
+		Case:    101,
+		Message: "'101' is greater than Maximum therefore should not be pass",
+		Pass:    false,
+	}}
+
+	for _, test := range tests {
+		ok := validator.Validate(test.Case)
+		//if Pass flag true, ok should be true
+		if test.Pass == true && ok != true {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, ok should not be true
+		if test.Pass == false && ok == true {
+			t.Error(test.Message)
+		}
+	}
+
+	//Case exclusive is true
+	validator, err = NewMaximumValidator(100, true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	tests = []MaximumValidatorTestCase{{
+		Case:    99,
+		Message: "'99' is less than Maximum therefore should be pass",
+		Pass:    true,
+	}, {
+		Case:    100,
+		Message: "'100' equal to Maximum and exclusive is false in this case should not be pass",
+		Pass:    false,
+	}, {
+		Case:    101,
+		Message: "'101' is greater than Maximum therefore should not be pass",
+		Pass:    false,
+	}}
+
+	for _, test := range tests {
+		ok := validator.Validate(test.Case)
+		//if Pass flag true, ok should be true
+		if test.Pass == true && ok != true {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, ok should not be true
+		if test.Pass == false && ok == true {
+			t.Error(test.Message)
+		}
+	}
+}
+
+func TestMinimumValidator(t *testing.T) {
+	//Case exclusive is false
+	validator, err := NewMinimumValidator(100, false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	tests := []MinimumValidatorTestCase{{
+		Case:    101,
+		Message: "'101' is greater than Minimum therefore should be pass",
+		Pass:    true,
+	}, {
+		Case:    100,
+		Message: "'100' equal to Minimum and exclusive is false in this case should be pass",
+		Pass:    true,
+	}, {
+		Case:    99,
+		Message: "'99' is less than Minimum therefore should not be pass",
+		Pass:    false,
+	}}
+
+	for _, test := range tests {
+		ok := validator.Validate(test.Case)
+		//if Pass flag true, ok should be true
+		if test.Pass == true && ok != true {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, ok should not be true
+		if test.Pass == false && ok == true {
+			t.Error(test.Message)
+		}
+	}
+
+	//Case exclusive is true
+	validator, err = NewMinimumValidator(100, true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	tests = []MinimumValidatorTestCase{{
+		Case:    101,
+		Message: "'101' is greater than Minimum therefore should be pass",
+		Pass:    true,
+	}, {
+		Case:    100,
+		Message: "'100' equal to Minimum and exclusive is false in this case should not be pass",
+		Pass:    false,
+	}, {
+		Case:    99,
+		Message: "'99' is less than Minimum therefore should not be pass",
+		Pass:    false,
+	}}
+
+	for _, test := range tests {
+		ok := validator.Validate(test.Case)
+		//if Pass flag true, ok should be true
+		if test.Pass == true && ok != true {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, ok should not be true
+		if test.Pass == false && ok == true {
+			t.Error(test.Message)
+		}
+	}
+}
+
+func TestMaxLength(t *testing.T) {
+	tests := []MaxLengthTestCase{{
+		Case:    MaxLengthValidator{MaxLength: -1},
+		Message: "MaxLength should be greater than 0",
+		Pass:    false,
+	}, {
+		Case:    MaxLengthValidator{MaxLength: 0},
+		Message: "MaxLength 0 should not be pass",
+		Pass:    false,
+	}, {
+		Case:    MaxLengthValidator{MaxLength: 1},
+		Message: "MaxLength 1 should be pass",
+		Pass:    true,
+	}}
+
+	for _, test := range tests {
+		_, err := NewMaxLengthValidator(test.Case.MaxLength)
+		//if Pass flag true, err should be empty
+		if test.Pass == true && err != nil {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, err should not be empty
+		if test.Pass == false && err == nil {
+			t.Error(test.Message)
+		}
+	}
+
+}
+
+func TestMaxLengthValidator(t *testing.T) {
+	validator, err := NewMaxLengthValidator(5)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	tests := []MaxLengthValidatorTestCase{{
+		Case:    "あいうえ",
+		Message: "4 charactors is less than maxLength therefore should be pass",
+		Pass:    true,
+	}, {
+		Case:    "あいうえお",
+		Message: "case value equal to maxLength should be pass",
+		Pass:    true,
+	}, {
+		Case:    "あいうえおか",
+		Message: "6 charactors is greater than maxLength therefore should not be pass",
+		Pass:    false,
+	}, {
+		Case:    "abcde",
+		Message: "case 5 characters in en should be pass",
+		Pass:    true,
+	}, {
+		Case:    "",
+		Message: "empty string should be pass",
+		Pass:    true,
+	}}
+
+	for _, test := range tests {
+		ok := validator.Validate(test.Case)
+		//if Pass flag true, ok should be true
+		if test.Pass == true && ok != true {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, ok should not be true
+		if test.Pass == false && ok == true {
+			t.Error(test.Message)
+		}
+	}
+}
+
+func TestMinLength(t *testing.T) {
+	tests := []MinLengthTestCase{{
+		Case:    MinLengthValidator{MinLength: -1},
+		Message: "MinLength should be greater than 0",
+		Pass:    false,
+	}, {
+		Case:    MinLengthValidator{MinLength: 0},
+		Message: "MinLength 0 should not be pass",
+		Pass:    false,
+	}, {
+		Case:    MinLengthValidator{MinLength: 1},
+		Message: "MinLength 1 should be pass",
+		Pass:    true,
+	}}
+
+	for _, test := range tests {
+		_, err := NewMinLengthValidator(test.Case.MinLength)
+		//if Pass flag true, err should be empty
+		if test.Pass == true && err != nil {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, err should not be empty
+		if test.Pass == false && err == nil {
+			t.Error(test.Message)
+		}
+	}
+}
+
+func TestMinLengthValidator(t *testing.T) {
+	validator, err := NewMinLengthValidator(5)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	tests := []MinLengthValidatorTestCase{{
+		Case:    "あいうえおか",
+		Message: "6 charactors is greater than minLength therefore should be pass",
+		Pass:    true,
+	}, {
+		Case:    "あいうえお",
+		Message: "case value equal to minLength should be pass",
+		Pass:    true,
+	}, {
+
+		Case:    "あいうえ",
+		Message: "4` charactors less than minLength therefore should not be pass",
+		Pass:    false,
+	}, {
+		Case:    "100 characters 100 characters 100 characters 100 characters 100 characters 100 characters 100 charac",
+		Message: "case 100 characters in en should be pass",
+		Pass:    true,
+	}, {
+		Case:    "",
+		Message: "empty string should not be pass",
+		Pass:    false,
+	}}
+
+	for _, test := range tests {
+		ok := validator.Validate(test.Case)
+		//if Pass flag true, ok should be true
+		if test.Pass == true && ok != true {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, ok should not be true
+		if test.Pass == false && ok == true {
+			t.Error(test.Message)
+		}
+	}
+}
+
+func TestPattern(t *testing.T) {
+	tests := []PatternTestCase{{
+		Case:    PatternValidator{Pattern: ""},
+		Message: "pattern should be empty",
+		Pass:    true,
+	}, {
+		Case:    PatternValidator{Pattern: "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z$"},
+		Message: "pattern `^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z$` should be pass regex compile",
+		Pass:    true,
+	}, {
+		Case:    PatternValidator{Pattern: "[a-z"},
+		Message: "pattern `[a-z` should not be pass",
+		Pass:    false,
+	}}
+
+	for _, test := range tests {
+		_, err := NewPatternValidator(test.Case.Pattern)
+		//if Pass flag true, err should be empty
+		if test.Pass == true && err != nil {
+			t.Error(test.Message)
+		}
+		//if Pass flag false, err should not be empty
+		if test.Pass == false && err == nil {
+			t.Error(test.Message)
+		}
+	}
+}
+
+func TestPatternValidator(t *testing.T) {
+	validator, err := NewPatternValidator("^(\\([0-9]{3}\\))?[0-9]{3}-[0-9]{4}$")
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	tests := []PatternValidatorTestCase{{
+		Case:    "555-1212",
+		Message: "should be pass",
+		Pass:    true,
+	}, {
+		Case:    "(888)555-1212",
+		Message: "should be pass",
+		Pass:    true,
+	}, {
+		Case:    "(888)555-1212 ext. 532",
+		Message: "should not be pass",
+		Pass:    false,
+	}, {
+		Case:    "(800)FLOWERS",
+		Message: "should not be pass",
+		Pass:    false,
+	}}
+	for _, test := range tests {
+		ok := validator.Validate(test.Case)
+		//if Pass flag true, ok should be true
+		if test.Pass == true && ok != true {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, ok should not be true
+		if test.Pass == false && ok == true {
+			t.Error(test.Message)
+		}
+	}
+}
+
+func TestIntEnum(t *testing.T) {
+	tests := []IntEnumTestCase{{
+		Case:    IntEnumValidator{Enumerate: []int{}},
+		Message: "enum accepts list shoud not be empty",
+		Pass:    false,
+	}, {
+		Case:    IntEnumValidator{Enumerate: []int{10, 20, 10}},
+		Message: "enum accepts element should be unique",
+		Pass:    false,
+	}, {
+		Case:    IntEnumValidator{Enumerate: []int{10, 20, 30}},
+		Message: "enum accepts list [10, 20, 30] should be pass",
+		Pass:    true,
+	}}
+
+	for _, test := range tests {
+		_, err := NewIntEnumValidator(test.Case.Enumerate)
+		//if Pass flag true, err should be empty
+		if test.Pass == true && err != nil {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, err should not be empty
+		if test.Pass == false && err == nil {
+			t.Error(test.Message)
+		}
+	}
+}
+
+func TestIntEnumvalidator(t *testing.T) {
+	validator, err := NewIntEnumValidator([]int{11, 13, 17, 19})
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	tests := []IntEnumValidatorTestCase{{
+		Case:    13,
+		Message: "There is '13' in enumerates therefore should be pass",
+		Pass:    true,
+	}, {
+		Case:    14,
+		Message: "There is not '14' in enumerates therefore should not be pass",
+		Pass:    false,
+	}}
+
+	for _, test := range tests {
+		ok := validator.Validate(test.Case)
+		//if Pass flag true, ok should be true
+		if test.Pass == true && ok != true {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, ok should not be true
+		if test.Pass == false && ok == true {
+			t.Error(test.Message)
+		}
+	}
+}
+
+func TestStringEnum(t *testing.T) {
+	tests := []StringEnumTestCase{{
+		Case:    StringEnumValidator{Enumerate: []string{}},
+		Message: "enum accepts list shoudl not be empty",
+		Pass:    false,
+	}, {
+		Case:    StringEnumValidator{Enumerate: []string{"test", "test"}},
+		Message: "enum accepts element should be unique",
+		Pass:    false,
+	}, {
+		Case:    StringEnumValidator{Enumerate: []string{"test1", "test2", "test3"}},
+		Message: "enum accepts list [test1, test2, test3] should be pass",
+		Pass:    true,
+	}}
+
+	for _, test := range tests {
+		_, err := NewStringEnumValidator(test.Case.Enumerate)
+		// if Pass flag true, err should be empty
+		if test.Pass == true && err != nil {
+			t.Error(test.Message)
+		}
+
+		// if Pass flag false, err should not be empty
+		if test.Pass != true && err == nil {
+			t.Error(test.Message)
+		}
+	}
+}
+
+func TestStringEnumvalidator(t *testing.T) {
+	validator, err := NewStringEnumValidator([]string{"red", "amgber", "blue"})
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	tests := []StringEnumValidatorTestCase{{
+		Case:    "red",
+		Message: "There is `red` in enumerates therefore should be pass",
+		Pass:    true,
+	}, {
+		Case:    "green",
+		Message: "There is not 'green' in enumerates therefore should not be pass",
+		Pass:    false,
+	}, {
+		Case:    "",
+		Message: "empty string case should not be pass",
+		Pass:    false,
+	}}
+
+	for _, test := range tests {
+		ok := validator.Validate(test.Case)
+		//if Pass flag true, ok should be true
+		if test.Pass == true && ok != true {
+			t.Error(test.Message)
+		}
+
+		//if Pass flag false, ok should not be true
+		if test.Pass == false && ok == true {
+			t.Error(test.Message)
+		}
+	}
+}
