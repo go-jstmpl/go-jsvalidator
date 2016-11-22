d60a660683c1a9068b16790950504a18a7a71773
fujiwara402 <fuj.t601216@gmail.com>
2016-11-22 17:47:23 +0900

    Add export variable for test

--- required_export_test.go
+++ required_export_test.go
@@ -4,4 +4,7 @@ var (
 	ConvertToConcreteValue = convertToConcreteValue
 	GetFieldByName         = getFieldByName
 	IsValid                = isValid
+	IsPresentArray         = isPresentArray
+	IsPresentString        = isPresentString
+	IsPresentStruct        = isPresentStruct
 )
